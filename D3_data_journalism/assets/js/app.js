0159aa94330d6c5f9bdc8f049891e3eb8ebb9aef
Scott Cerny <scottcerny@yahoo.com>
2021-07-12 10:05:27 -0700

    loading csv file via app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,33 @@
 // @TODO: YOUR CODE HERE!
+// Load data from csv
+d3.csv("assets/data/data.csv").then(function(dataJournalism) {
+
+    console.log(dataJournalism);
+  
+    // log a list of states
+    var states = dataJournalism.map(data => data.state);
+    console.log("state", states);
+
+    // log a list of state abbreviations
+    var stateAbbr = dataJournalism.map(data => data.abbr);
+    console.log("abbr", stateAbbr);
+
+    // log a list of healthcare
+    var healthCare = dataJournalism.map(data => data.healthcare);
+    console.log("healthcare", healthCare);
+
+    // log a list of states poverty
+    var statePov = dataJournalism.map(data => data.poverty);
+    console.log("poverty", statePov);
+    
+  
+    // Cast each healthcare value in csv as a number using the unary + operator
+    dataJournalism.forEach(function(data) {
+      data.healthcare = +data.healthcare;
+      console.log("State:", data.state);
+      console.log("Health Care:", data.healthcare);
+    });
+  }).catch(function(error) {
+    console.log(error);
+  });
+  
